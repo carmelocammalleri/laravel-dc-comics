3abb2b34823c1f9f9de54479b06768dbe219ace6
Melozzo <carmecamma1995@gmail.com>
2023-11-23 18:09:23 +0100

    [add&change]: root

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>ciao bello</h1>
+</body>
+</html>
