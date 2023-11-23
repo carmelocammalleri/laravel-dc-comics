f55120d1a8cc85e2d04104143f19c36db3f0c5ed
Melozzo <carmecamma1995@gmail.com>
2023-11-23 18:10:35 +0100

    [create]: model

---
+++ app/Models/Guest/Comic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Guest;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    use HasFactory;
+}
