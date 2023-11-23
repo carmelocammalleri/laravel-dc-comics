8590c87082810b695f5e7d883db9e3ebbe2ec26b
Melozzo <carmecamma1995@gmail.com>
2023-11-23 18:23:51 +0100

    [create]: foreach and link comic model

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use app\Models\Guest\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -15,5 +16,17 @@ class ComicsTableSeeder extends Seeder
     public function run()
     {
         $comics = config('comics');
+
+        foreach ($comics as $comic) {
+
+            $new_comic = new Comic();
+                $new_comic->title = $comic['title'];
+                $new_comic->description = $comic['description'];
+                $new_comic->thumb = $comic['thumb'];
+                $new_comic->price = $comic['price'];
+                $new_comic->series = $comic['series'];
+                $new_comic->sale_date = $comic['sale_date'];
+                $new_comic->type = $comic['type'];
+        }
     }
 }
