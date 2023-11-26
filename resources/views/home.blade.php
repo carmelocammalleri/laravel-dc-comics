d5e0af09b06df5942349c76d04224623865f21c5
Melozzo <carmecamma1995@gmail.com>
2023-11-26 18:34:04 +0100

    [change]: structure of webpage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+@extends('layouts.main')
 
-        <!-- Styles -->
-        @vite('resources/js/app.js')
-</head>
-    <body>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-            </tr>
-            </thead>
+@section('content')
+    <main>
 
-            <tbody>
-                @foreach ($comics as $comic )
+        <h1>ciao sono il main</h1>
+    </main>
 
-                    <tr>
-                        <th scope="row">{{$comic->title}}</th>
-                        <td>{{$comic->description}}</td>
-                        <td>{{$comic->price}}</td>
-                        <td>{{$comic->series}}</td>
-                    </tr>
-
-                @endforeach
-            </tbody>
-        </table>
-    </body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,5 @@ use App\Http\Controllers\PageController;
 */
 
 Route::get("/", [PageController::class,"index"])->name('home');
+
+Route::resource('comics',ComicController::class);
