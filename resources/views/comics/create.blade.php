ed98804fa3287757e1e9d8663b9b587b220cf8f4
Melozzo <carmecamma1995@gmail.com>
2023-11-26 20:57:51 +0100

    [create]: forms elements

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
+<div>
+    <h1>Aggiungi fumetto</h1>
 
-<h1>aggiungi fumetto</h1>
+ <div class="row">
+    <div class="col-8">
+        <form action="{{ route('comics.store') }}" method="POST">
+
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Nome Fumetto</label>
+                <input type="text" class="form-control" id="title" name="title" >
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Immagine</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" >
+            </div>
+            <div class="mb-3">
+                <label for="description">Descrizione</label>
+                <textarea class="form-control" id="description" name="description" style="height: 200px"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="price" class="form-control" id="price" name="price" >
+            </div>
+
+            <button type="submit" class="btn btn-secondary">Invia</button>
+            <button type="reset" class="btn btn-danger">Annulla</button>
+
+        </form>
+    </div>
+ </div>
+</div>
+
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <main class="container">
 
         <h1>ciao sono la home</h1>
+        <a href="{{ route('comics.create')}}" class="btn btn-primary">Aggiungi un fumetto</a>
     </main>
 
 @endsection
