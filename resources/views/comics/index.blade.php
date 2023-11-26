029cdfadd10e9d2fba3774cf9cfb2298596912af
Melozzo <carmecamma1995@gmail.com>
2023-11-26 19:51:35 +0100

    [add]: fontawesome

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
 
+    <h1>Ci sono a tua disposizione {{count($comics)}} fumetti</h1>
+    <div class="container d-flex flex-wrap">
 
-    @foreach ($comics as $comic )
-        <div class="card" style="width: 18rem;">
-            <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
-            <div class="card-body">
-            <h5 class="card-title">{{$comic->title}}</h5>
-            <p class="card-text">{{$comic->description}}</p>
-            <p>{{$comic->price}}</p>
-            <a href="#" class="btn btn-primary">dettagli</a>
+        @foreach ($comics as $comic )
+            <div class="card m-2" style="width: 18rem;">
+                <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                <div class="card-body">
+                <h5 class="card-title">{{$comic->title}}</h5>
+                <p class="card-text">{{$comic->description}}</p>
+                <p>{{$comic->price}}</p>
+                <a href="#" class="btn btn-primary">dettagli</a>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 
 
 
